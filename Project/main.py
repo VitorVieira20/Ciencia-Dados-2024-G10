efe43da94a63b8d3dfae4d8b3f7ca740e50fb361
VitorVieira20 <2078320@student.uma.pt>
2024-02-25 10:49:06 +0000

    feat(CleanData): convert size column values to a float number in a new column and convert Sqr. Meter and Sqr: Yard to Sqr. Feet in the total_sqft column, also made the average when the data were a range

--- Project/main.py
+++ Project/main.py
@@ -1,4 +1,3 @@
-#%% Visualization
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -21,18 +20,71 @@ class CleanData:
         :return:
         '''
 
-        #Read the file passed in constructor
+        # Read the file passed in constructor
         df = pd.read_csv(self.data)
 
-        #Drop unnecessary columns
+        # Drop unnecessary columns
         df_drop_cols = df.drop(['area_type','society','availability'],axis='columns')
 
-        #Drop null values
+        # Drop null values
         df_drop_null = df_drop_cols.dropna()
 
-        # Return the cleaned DataFrame
-        return df_drop_null
+        # In the column siz there are two types of size
+        # BHK and Bedroom, so were going to add a column with those data simplified
+        df_drop_null['bedroom'] = df_drop_null['size'].apply(lambda x: int(x.split(' ')[0]))
 
+        # Drop size column because we have now the bedroom column
+        df_drop_size = df_drop_null.drop(['size'], axis='columns')
+
+        # In the column total_sqt are many types of values
+        # ->Single values: 2400
+        # ->Range values: 2100 - 2600
+        # ->Values in Sqr Meter: 34.46Sq. Meter
+        # ->Values in Sqr Yards: 151.11Sq. Yards
+        # So were gonna make all of them in a single value like the first example
+        df_range_ave = df_drop_size.copy()
+        df_range_ave['total_sqft'] = df_drop_size['total_sqft'].apply(self._range_average)
+
+        print(df_range_ave.loc[410])
+        print(df_range_ave.loc[8273])
+
+        # Return the cleaned DataFrame
+        return df_drop_size
+
+    def _range_average(self, x):
+        '''
+        Calculates the average of a given range like 2100 - 2500
+        :param x:
+        :return:
+        '''
+        tokens = x.split('-')
+        if len(tokens) == 2:
+            return (float(tokens[0]) + float(tokens[1])) / 2
+        else:
+            # 142.84Sq. Meter
+            # 300Sq. Yards
+            # 6Acres
+            # 38Guntha
+            # 1Grounds
+            # 1500Cents
+            # 4125Perch
+            if 'Sq. Meter' in x:
+                # Split the Sq. Meter text from the number
+                numeric_part = x.split('Sq. Meter')[0].strip()
+                numeric_value = ''.join(filter(lambda char: char.isdigit() or char == '.', numeric_part))
+                if numeric_value:
+                    sqm_value = float(numeric_value)
+                    sqft_value = sqm_value * 10.7639
+                    return sqft_value
+            elif 'Sq. Yards' in x:
+                # Split the Sq. Yards text from the number
+                numeric_part = x.split('Sq. Yards')[0].strip()
+                numeric_value = ''.join(filter(lambda char: char.isdigit() or char == '.', numeric_part))
+                if numeric_value:
+                    sqy_value = float(numeric_value)
+                    sqft_value = sqy_value * 9
+                    return sqft_value
+            return None
 
 
 if __name__ == '__main__':
